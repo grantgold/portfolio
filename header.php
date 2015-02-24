5d5706003df3dbb33d43d8afd7cbf6f0801300d1
grantgold <grantagold@gmail.com>
2015-02-24 10:32:05 -0500

    Initial file commit

---
+++ header.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>Grant Gold</title>
+
+    <meta name="keywords" content="Grant Gold, Designer, Portfolio, New York, The New York Times, Google, Rdio, Squarespace, MAD, Copenhagen" />
+    
+    <meta name="description" content="Portfolio" />
+
+    <link rel="profile" href="http://gmpg.org/xfn/11" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <!-- Fonts: Lato / http://www.latofonts.com/ -->
+    <link href='http://fonts.googleapis.com/css?family=Lato:100,300,400,700,900,100italic,300italic,400italic,700italic,900italic' rel='stylesheet' type='text/css'>
+
+    <!--Google Analytics -->
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" type="text/css" href="css/bootstrap.min.css"/>
+      
+    <!-- Le Font Awesome // http://fortawesome.github.io/Font-Awesome/icons -->
+    <link rel="stylesheet" type="text/css" href="css/font-awesome.min.css"/>
+      
+    <!-- Custom CSS -->
+    <link rel="stylesheet" type="text/css" media="screen, print" href="style.css"/>
+
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
+
+</head>
+<body>
---
+++ index.php
@@ -0,0 +1,19 @@
+<?php include('header.php'); ?>
+
+<div id="list">
+	<div class="container">
+		<b>Grant Gold</b> is a designer in New York City
+	</div>
+	<div class="container">
+		<b>Work to date:</b>
+		<ul>
+			<li>Design, interaction, and product at <a href="http://www.nytimes.com" target="_blank"><i>The New York Times</i></a>.</li>
+			<li>Full UI/UX redesign of all mobile products at <a href="http://www.rdio.com" target="_blank"><i>rdio</i></a> on iPhone, iPad, and Android.</li>
+			<li>Product direction, design, interaction, and animation at <a href="http://www.squarespace.com" target="_blank"><i>Squarespace</i></a>.</li>
+			<li>Design, 
+		</ul>
+	</div>
+</div>
+
+
+<?php include('footer.php'); ?>

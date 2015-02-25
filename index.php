99193745a1b0dfcb36407d17fe57eacafb33e7a0
grantgold <grantagold@gmail.com>
2015-02-25 08:24:40 -0500

    Adds colors

--- index.php
+++ index.php
@@ -8,6 +8,7 @@
 		<b>Work to date:</b>
 		<ul>
 			<li>Design, interaction, and product at <a href="http://www.nytimes.com" target="_blank"><i>The New York Times</i></a>.</li>
+			<li>Identity, design, interaction, and development for <a href="http://madfeed.co" target="_blank"><i>MAD</i></a>.</li>
 			<li>Full UI/UX redesign of all mobile products at <a href="http://www.rdio.com" target="_blank"><i>rdio</i></a> on iPhone, iPad, and Android.</li>
 			<li>Product direction, design, interaction, and animation at <a href="http://www.squarespace.com" target="_blank"><i>Squarespace</i></a>.</li>
 			<li>Design, 

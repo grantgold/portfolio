5786e4dc205eaee688328c0f40475ae08892b167
grantgold <grantagold@gmail.com>
2015-04-07 10:15:55 -0400

    Modified links

--- index.php
+++ index.php
@@ -8,7 +8,7 @@
 		<b>Work to date:</b>
 		<ul>
 			<li>Design, interaction, and product at <a href="http://www.nytimes.com" target="_blank">The New York Times</a>.</li>
-			<li>Styling, design, interaction, and development for <a href="#">MAD</a>.</li>
+			<li>Styling, design, interaction, and development for <a href="http://www.madfeed.co">MAD</a>.</li>
 			<li>Full UI/UX redesign of all mobile products at <a href="http://www.rdio.com" target="_blank">rdio</a> on iPhone, iPad, and Android.</li>
 			<li>Product features, design and interaction of themes and mobile apps at <a href="http://www.squarespace.com" target="_blank">Squarespace</a>.</li>
 			<li>System and UI design for a responsive overhaul of Gmail, Search, Maps, Calendar, and Docs at <a href="http://www.google.com" target="_blank">Google</a>.</li>

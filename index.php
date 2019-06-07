aff1ee512b9f73e2c61364be5b7cb1aa3945ab59
Grant Gold <grantgold@grants-mbp.attlocal.net>
2019-06-07 12:31:32 -0700

    new material

--- index.php
+++ index.php
@@ -1,17 +1,18 @@
 <?php include('header.php'); ?>
 
-<div id="resume">
+<div id="dailies">
+	<div id="notebook" class="notebook">
+		<div class="hs full">
+			<div class="card"><img class="photo" src="image_host/2019/DSCF2710.jpg"><li class="entry"><span class="date">Day 54:</span> The story in crowds of marker caps, stalky, short, tall colorful of the back-to 24 pack. Red collared and hidden behind a misshapen, penciled cloth.</li></div>
+			<div class="card"><img class="photo" src="image_host/2019/DSCF2890.jpg"><li class="entry"><span class="date">Day 53:</span> I once enjoyed employment but I am become breath. One of the invisible, the wind watchers. Holder of a single key, sometimes brass, sometimes silver.</li></div>
+			<div class="card"><img class="photo" src="image_host/2019/DSCF2993.jpg"><li class="entry"><span class="date">Day 52:</span> Crew-cutted swimming instructor singing great grandma's camp song. Something something 5 foot 2, could she chould she coochie koo. Differing between a pool and a bath.</li></div>
+			<div class="card"><img class="photo" src="image_host/2019/DSCF2854.jpg"><li class="entry"><span class="date">Day 37:</span> Someday a large blackbird, shooting out from behind your parked car, wrestling winds over the desert rock and dropping away into a canyon. Someday a sandy sagebrush, smelling goes away, hardens, dries out.</li></div>
+			<div class="card"><img class="photo" src="image_host/2019/DSCF1924.jpg"><li class="entry"><span class="date">Day 18:</span> He would have one of those bridges you can't even tell you're going over if he could. Real slate too, not that cheap crap that gets moldy every few years. Oohing over the blue paint on a jaguar.</li></div>
+
+	</div>
 	<div class="container">
 		<b>Grant Gold</b><br>
-		<li>Photography, writing, reading, traveling</li>
-	</div>
-	<div id="list" class="container">
-		<ul>
-			<li>I once enjoyed employment but I am become gone. One of the invisible, the wind watchers, holder of a single key that changes shape daily.</li>
-			<li>Available for collaboration.</li>
-		</ul>
-	</div>
-	<div class="container">
+		<li>Photographs and writing.</li>
 		<li><script type="text/javascript"><!--
 			var yceipdf = ['o','l','o','l','a','i','"','a','"','a','i','a',' ','/','t','a','l','o','>','e','m','g','g','"','h','@','m','l','n','l','a','c','f','c','s','a','t','=','g','e','d','m','g','m','=','m','>','l','r','i','o','<','i','.','d','g',' ','o','a',':','<','a','r','@','l','.','r','s','a','m','t','"','c','a','g','n'];var qgrxvld = [14,39,70,12,74,30,36,18,8,10,66,21,2,73,56,40,60,34,75,5,35,22,52,50,3,62,9,49,55,24,29,38,6,69,42,54,13,7,63,45,25,71,58,64,43,46,51,67,53,48,23,0,11,32,61,27,37,59,47,15,72,57,17,26,31,68,4,41,65,28,20,44,33,1,16,19];var lknzanq= new Array();for(var i=0;i<qgrxvld.length;i++){lknzanq[qgrxvld[i]] = yceipdf[i]; }for(var i=0;i<lknzanq.length;i++){document.write(lknzanq[i]);}
   // --></script>

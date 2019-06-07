16c85a633cc2b24378c5bc6df4aefb731c8eae93
Grant Gold <grantgold@grants-mbp.attlocal.net>
2019-06-07 09:13:08 -0700

    style fix

--- index.php
+++ index.php
@@ -3,21 +3,21 @@
 <div id="resume">
 	<div class="container">
 		<b>Grant Gold</b><br>
-		Photography, writing, reading, traveling
+		<li>Photography, writing, reading, traveling</li>
 	</div>
 	<div id="list" class="container">
-		<b>Latest:</b>
 		<ul>
 			<li>I once enjoyed employment but I am become gone. One of the invisible, the wind watchers, holder of a single key that changes shape daily.</li>
 			<li>Available for collaboration, not presently working for money. Just experience and friendships.</li>
 		</ul>
 	</div>
 	<div class="container">
-		<script type="text/javascript"><!--
+		<li><script type="text/javascript"><!--
 			var yceipdf = ['o','l','o','l','a','i','"','a','"','a','i','a',' ','/','t','a','l','o','>','e','m','g','g','"','h','@','m','l','n','l','a','c','f','c','s','a','t','=','g','e','d','m','g','m','=','m','>','l','r','i','o','<','i','.','d','g',' ','o','a',':','<','a','r','@','l','.','r','s','a','m','t','"','c','a','g','n'];var qgrxvld = [14,39,70,12,74,30,36,18,8,10,66,21,2,73,56,40,60,34,75,5,35,22,52,50,3,62,9,49,55,24,29,38,6,69,42,54,13,7,63,45,25,71,58,64,43,46,51,67,53,48,23,0,11,32,61,27,37,59,47,15,72,57,17,26,31,68,4,41,65,28,20,44,33,1,16,19];var lknzanq= new Array();for(var i=0;i<qgrxvld.length;i++){lknzanq[qgrxvld[i]] = yceipdf[i]; }for(var i=0;i<lknzanq.length;i++){document.write(lknzanq[i]);}
   // --></script>
 			<noscript>Please enable Javascript to see the email address</noscript>
 			<a class="noHighlight" href="https://instagram.com/grantagold/" target="_blank">/ Instagram</a>
+		</li>
 	</div>
 </div>
 

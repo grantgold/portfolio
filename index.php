51436d00227cb288c9794300e6fe1cbc22cef9c5
Grant Gold <grantgold@grants-mbp.attlocal.net>
2019-06-07 09:09:21 -0700

    update

--- index.php
+++ index.php
@@ -3,16 +3,13 @@
 <div id="resume">
 	<div class="container">
 		<b>Grant Gold</b><br>
-		Art Director at The New York Times
+		Photography, writing, reading, traveling
 	</div>
 	<div id="list" class="container">
-		<b>Work to date:</b>
+		<b>Latest:</b>
 		<ul>
-			<li>Art direction and design of newsroom editorial at <a href="http://www.nytimes.com" target="_blank">The New York Times</a>.</li>
-			<li>Direction, design, and development for Chef Ren&#233; Redzepi&#8217;s non-pro&#64257;t, <a href="http://www.madfeed.co">MAD</a>.</li>
-			<li>UI/UX design of mobile products at <a id="link" href="http://www.rdio.com" target="_blank">rdio</a> on iPhone, iPad, and Android.</li>
-			<li>Product features, design and interaction of themes and mobile apps at <a href="http://www.squarespace.com" target="_blank">Squarespace</a>.</li>
-			<li>The first large-scale redesign of Gmail, Search, Maps, Calendar, and Docs at <a href="http://www.theverge.com/2013/1/24/3904134/google-redesign-how-larry-page-engineered-beautiful-revolution" target="_blank">Google</a>.</li>
+			<li>I once enjoyed employment but I am become gone. One of the invisible, the wind watchers, holder of a single key that changes shape daily.</li>
+			<li>Available for collaboration, not presently working for money. Just experience and friendships.</li>
 		</ul>
 	</div>
 	<div class="container">
@@ -20,8 +17,7 @@
 			var yceipdf = ['o','l','o','l','a','i','"','a','"','a','i','a',' ','/','t','a','l','o','>','e','m','g','g','"','h','@','m','l','n','l','a','c','f','c','s','a','t','=','g','e','d','m','g','m','=','m','>','l','r','i','o','<','i','.','d','g',' ','o','a',':','<','a','r','@','l','.','r','s','a','m','t','"','c','a','g','n'];var qgrxvld = [14,39,70,12,74,30,36,18,8,10,66,21,2,73,56,40,60,34,75,5,35,22,52,50,3,62,9,49,55,24,29,38,6,69,42,54,13,7,63,45,25,71,58,64,43,46,51,67,53,48,23,0,11,32,61,27,37,59,47,15,72,57,17,26,31,68,4,41,65,28,20,44,33,1,16,19];var lknzanq= new Array();for(var i=0;i<qgrxvld.length;i++){lknzanq[qgrxvld[i]] = yceipdf[i]; }for(var i=0;i<lknzanq.length;i++){document.write(lknzanq[i]);}
   // --></script>
 			<noscript>Please enable Javascript to see the email address</noscript>
-			<a class="noHighlight" href="https://twitter.com/grantagold" target="_blank">/ twitter</a>
-			<a class="noHighlight" href="https://instagram.com/cultofjfulford/" target="_blank">/ Instagram</a>
+			<a class="noHighlight" href="https://instagram.com/grantagold/" target="_blank">/ Instagram</a>
 	</div>
 </div>
 

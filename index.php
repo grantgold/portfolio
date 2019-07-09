d2662dd70cc55cdc2bf199c8487b8a7307ee6b57
Grant Gold <grantagold@gmail.com>
2019-07-09 17:22:57 -0500

    update

--- index.php
+++ index.php
@@ -1,28 +1,30 @@
 <?php include('header.php'); ?>
 
 <div id="dailies">
+	<div class="top">
+		<div class="title">
+			<b>Grant Gold</b>
+			<div class="description">Photographs and writing.</div>
+		</div>
+		<div class="links"><li><script type="text/javascript"><!--
+			var yceipdf = ['o','l','o','l','a','i','"','a','"','a','i','a',' ','/','t','a','l','o','>','e','m','g','g','"','h','@','m','l','n','l','a','c','f','c','s','a','t','=','g','e','d','m','g','m','=','m','>','l','r','i','o','<','i','.','d','g',' ','o','a',':','<','a','r','@','l','.','r','s','a','m','t','"','c','a','g','n'];var qgrxvld = [14,39,70,12,74,30,36,18,8,10,66,21,2,73,56,40,60,34,75,5,35,22,52,50,3,62,9,49,55,24,29,38,6,69,42,54,13,7,63,45,25,71,58,64,43,46,51,67,53,48,23,0,11,32,61,27,37,59,47,15,72,57,17,26,31,68,4,41,65,28,20,44,33,1,16,19];var lknzanq= new Array();for(var i=0;i<qgrxvld.length;i++){lknzanq[qgrxvld[i]] = yceipdf[i]; }for(var i=0;i<lknzanq.length;i++){document.write(lknzanq[i]);}
+  // --></script>
+			<noscript>Please enable Javascript to see the email address</noscript>
+			/ <a class="noHighlight" href="https://instagram.com/grantagold/" target="_blank">Instagram</a></li>
+		</div>
+	</div>
 	<div id="notebook" class="notebook">
 		<div class="hs">
-			<div class="card"><img class="photo" src="image_host/2019/DSCF3778.jpg"><li class="entry"><span class="date">Day 84:</span> Sinking hands and covering my fingers deep down. The room I used to hide in, it had soft plush carpet. Bougie 70’s wood paneled desk with hidden pornographic playing cards. Spiraling coil of brass, flickered with spring and a catapulted pop, one of many door stoppers I entertained myself with in the room where no one screamed or made fun of each other, called “the front bedroom.”</li></div>
+			<div class="card"><img class="photo" src="image_host/2019/DSCF3778.jpg"><li class="entry"><span class="date">Day 84:</span> Sinking hands cover my fingers deep down. The room I used to hide in, it had soft plush carpet. Bougie 70’s wood paneled desk with hidden pornographic playing cards. Spiraling coil of brass, flickered with spring and a catapulted pop, one of many door stoppers I entertained myself with in the room where no one screamed or made fun of each other, named with affection “the front bedroom.”</li></div>
 			<div class="card"><img class="photo" src="image_host/2019/DSCF3581.jpg"><li class="entry"><span class="date">Day 76:</span> "It is a joy to be hidden and a disaster not to be found." – D.W. Winnicott</li></div>
-			<div class="card"><img class="photo" src="image_host/2019/DSCF3084.jpg"><li class="entry"><span class="date">Day 57:</span> Where is that skinny leg syndrome, got you so shook-a-look, can’t see knobs on those knees. Pale white lank and uncut hair, mosquito bite worries but ample showing lax in public.</li></div>
-			<div class="card"><img class="photo" src="image_host/2019/DSCF2710.jpg"><li class="entry"><span class="date">Day 54:</span> The story in crowds of marker caps, stalky, short, tall colorful of the back-to 24 pack. Red collared and hidden behind a misshapen, penciled cloth.</li></div>
+			<div class="card"><img class="photo" src="image_host/2019/DSCF3084.jpg"><li class="entry"><span class="date">Day 57:</span> My pet snake, he bit me. I called him Steve, desiring mediocre protection from my bulleyable title, Grant. Waking up a child, fearing a name.</li></div>
+			<div class="card"><img class="photo" src="image_host/2019/DSCF2710.jpg"><li class="entry"><span class="date">Day 54:</span> Safety fabric, sheets despite weight or thickness, hide me from everything but scorpions.</li></div>
 			<div class="card"><img class="photo" src="image_host/2019/DSCF2890.jpg"><li class="entry"><span class="date">Day 53:</span> I once enjoyed employment but I am become breath. One of the invisible, the wind watchers. Holder of a single key, sometimes brass, sometimes silver.</li></div>
-			<div class="card"><img class="photo" src="image_host/2019/DSCF2993.jpg"><li class="entry"><span class="date">Day 52:</span> Crew-cutted swimming instructor singing great grandma's camp song. Something something 5 foot 2, could she chould she coochie koo. Differing between a pool and a bath.</li></div>
+			<div class="card"><img class="photo" src="image_host/2019/DSCF2993.jpg"><li class="entry"><span class="date">Day 52:</span> Crew-cutted swimming instructor singing great grandma's camp song. Something something 5 foot 2, could she chould she coochie koo. Smile to say hello, frown of walk away.</li></div>
 			<div class="card"><img class="photo" src="image_host/2019/DSCF2022.jpg"><li class="entry"><span class="date">Day 50:</span> Grown tall. Age of “I spent my whole life trying not to let my emotions get in the way, only now I wonder why I can’t feel anything,” age of unripe white peach, age of indiscernible source of smoke, ash, siren. He thinks it’s ok but it is not.</li></div>
 			<div class="card"><img class="photo" src="image_host/2019/DSCF2854.jpg"><li class="entry"><span class="date">Day 37:</span> Someday a large blackbird, shooting out from behind your parked car, wrestling winds over the desert rock and drips away into a canyon. Someday, age 14 drives up the stairs on a dirt bike, hit the gas but aimed for the break. Someday crust scraped from your scalp, a pan of oil, juicing white nectarine. Someday that man has a boob mug full of toenails.</li></div>
 			<div class="card"><img class="photo" src="image_host/2019/DSCF1924.jpg"><li class="entry"><span class="date">Day 18:</span> He would have one of those bridges you can't even tell you're going over if he could. Real slate too, not that cheap crap that gets moldy every few years. Oohing over the blue paint on a jaguar.</li></div>
 
-	</div>
-	<div class="container">
-		<b>Grant Gold</b><br>
-		<li>Photographs and writing.</li>
-		<li><script type="text/javascript"><!--
-			var yceipdf = ['o','l','o','l','a','i','"','a','"','a','i','a',' ','/','t','a','l','o','>','e','m','g','g','"','h','@','m','l','n','l','a','c','f','c','s','a','t','=','g','e','d','m','g','m','=','m','>','l','r','i','o','<','i','.','d','g',' ','o','a',':','<','a','r','@','l','.','r','s','a','m','t','"','c','a','g','n'];var qgrxvld = [14,39,70,12,74,30,36,18,8,10,66,21,2,73,56,40,60,34,75,5,35,22,52,50,3,62,9,49,55,24,29,38,6,69,42,54,13,7,63,45,25,71,58,64,43,46,51,67,53,48,23,0,11,32,61,27,37,59,47,15,72,57,17,26,31,68,4,41,65,28,20,44,33,1,16,19];var lknzanq= new Array();for(var i=0;i<qgrxvld.length;i++){lknzanq[qgrxvld[i]] = yceipdf[i]; }for(var i=0;i<lknzanq.length;i++){document.write(lknzanq[i]);}
-  // --></script>
-			<noscript>Please enable Javascript to see the email address</noscript>
-			<a class="noHighlight" href="https://instagram.com/grantagold/" target="_blank">/ Instagram</a>
-		</li>
 	</div>
 </div>
 
